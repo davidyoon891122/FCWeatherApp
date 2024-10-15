b7f983da3dadde93a962109386adbe0c2744a3ba
David Yoon <qqpo12@naver.com>
2024-10-15 21:14:19 +0900

    Add a Widget
    * Added a widget provider
    * Added a widget info and widget layout

---
+++ app/src/main/java/com/example/fcweatherapp/WeatherAppWidgetProvider.kt
@@ -0,0 +1,36 @@
+package com.example.fcweatherapp
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class WeatherAppWidgetProvider: AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        appWidgetIds.forEach { appWidgetId ->
+            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                .let { intent ->
+                    PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+                }
+
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.widget_weather
+            ).apply {
+                setOnClickPendingIntent(R.id.testTextView, pendingIntent)
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+
+
+    }
+}
